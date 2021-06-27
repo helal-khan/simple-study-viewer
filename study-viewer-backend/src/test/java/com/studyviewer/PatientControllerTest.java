aac9156332aadb1af5559b1ce4a7479f2aa43702
helal <helal.khan@adndiginet.com>
2021-06-28 03:48:00 +0600

    Simple test file added.

---
+++ study-viewer-backend/src/test/java/com/studyviewer/PatientControllerTest.java
@@ -0,0 +1,18 @@
+package com.studyviewer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PatientControllerTest {
+
+    @Test
+    @DisplayName("Should Create Contact")
+    public void shouldCreateContact() {
+        //ContactManager contactManager = new ContactManager();
+        //contactManager.addContact("John", "Doe", "0123456789");
+        //assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, 1);
+    }
+}
